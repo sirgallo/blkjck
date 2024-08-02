b6e9584d9b1b00636e99f914626392871b442d63
Sir Gallo <nickgallo97@gmail.com>
2024-08-02 16:16:00 -0400

    wtf formatting

--- program/src/state/table.rs
+++ program/src/state/table.rs
@@ -30,9 +30,9 @@ pub struct Chair {
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Table {
-  // pub owner: Pubkey, // pub key generated as program derived address --> this has to be owner, weird enforcement for pdas, just make implicit 
-  pub table_creator: Pubkey, // pub key associated with the wallet/user that owns the repository
-  pub table_name: String,
+	// pub owner: Pubkey, // pub key generated as program derived address --> this has to be owner, weird enforcement for pdas, just make implicit 
+	pub table_creator: Pubkey, // pub key associated with the wallet/user that owns the repository
+	pub table_name: String,
 	pub table_deck: Deck, // the deck also contains the current round and the status of the round
 	pub table_dealer: Vec<Card>,
 	pub table_players: DataMap<Pubkey, Chair>
